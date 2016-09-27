c4b636231b35550f0613891e8307927fe4cd040d
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-09-27 01:03:54 +0100

    reworded help messages

--- recipy/cmd_search.py
+++ recipy/cmd_search.py
@@ -9,20 +9,20 @@ from recipy.cli import pass_config
               help='Match based on a fragment of run id.')
 @click.option('--filepath', '-f', is_flag=True,
               help='Match all files from the same directory as FILENAME.')
-@click.option('--all', '-a', is_flag=True, help='Return all results.')
+@click.option('--all', '-a', is_flag=True, help='Return all runs.')
 @click.option('--json', '-j', is_flag=True, help='Return results as JSON.')
 @click.option('--inputs', is_flag=True, help='Restrict search to run inputs only.')
 @click.option('--outputs', is_flag=True, help='Restrict search to run outputs only.')
 @click.argument('filename', required=True)
 @pass_config
 def cmd(config, exact, id, filepath, all, json, filename, inputs, outputs):
-    """Search for FILENAME or part of it in outputs and inputs of all runs
-    recorded by ReciPy. You can use regular expressions in FILENAME.
-    By default it only returns the most recent result.
+    """Search for FILENAME among all inputs and outputs recorded by ReciPy.
+    By default, it returns partial matches and most recent run. You can
+    use regular expressions in FILENAME.
 
     \b
-    For example: recipy search -a --outputs new_plot
-    Will find all runs which outputs had a file containing
-    new_plot in its name.
+    For example: recipy search -a --outputs newplot
+    Will look through outputs of all runs and try to find a file containing
+    newplot in its name.
     """
     click.echo('Searching...')
